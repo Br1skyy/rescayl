4e045329e6cdba4fd513a5a40b4a703ed225e802
Br1skyy_ <thunderpop697@gmail.com>
2026-08-18 17:01:40 -0700

    fix: render webview on Linux via WEBKIT_DISABLE_DMABUF_RENDERER

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // Work around blank/gray rendering on some GPUs (NVIDIA + others).
+    // Must be set before the WebKit webview is created.
+    #[cfg(target_os = "linux")]
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+
     rescayl_lib::run()
 }
